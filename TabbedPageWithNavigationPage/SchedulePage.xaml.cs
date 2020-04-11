97762f9ba0d8920c6babb707753233f98ef27d57
asnopek <asnopek486@gmail.com>
2020-04-11 18:02:55 -0500

    Add project files.

---
+++ TabbedPageWithNavigationPage/SchedulePage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TabbedPageWithNavigationPage
+{
+	public partial class SchedulePage : ContentPage
+	{
+		public static string result;
+		public SchedulePage ()
+		{
+			InitializeComponent ();
+		}
+
+		public void CalculateYears(Object sender, EventArgs args)
+		{
+			int numberEntry = Convert.ToInt32(Age.Text);
+			var currentDate = DateTime.Now;
+			var currentYear = currentDate.Year;
+
+			if(currentYear - numberEntry > 16)
+			{
+				result = currentYear - numberEntry - 16 + "";
+				Output.Text = "You have been able to drive legally for " + result + " years.";
+			}
+			else if(currentYear - numberEntry == 16)
+			{
+				result = "0";
+				Output.Text = "You have been able to drive legally for " + result + " years.";
+			}
+			else
+			{
+				Output.Text = "You are not legally old enough to drive in Wisconsin.";
+			}
+
+		}
+	}
+}
+
---
+++ TabbedPageWithNavigationPage/SettingsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TabbedPageWithNavigationPage
+{
+	public partial class SettingsPage : ContentPage
+	{
+		public SettingsPage ()
+		{
+			InitializeComponent ();
+		}
+
+		async void SurpriseModal(object sender, EventArgs args)
+		{
+			await Navigation.PushModalAsync(new UpcomingAppointmentsPage(), false);
+		}
+	}
+}
